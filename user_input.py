169edf006f6ab9a1851610722378f5f5b5c1e7d1
wreid01 <47088472+wreid01@users.noreply.github.com>
2019-05-05 20:39:48 -0400

    Update user_input.py

--- user_input.py
+++ user_input.py
@@ -9,20 +9,26 @@ import numpy as np
 import tkinter as tk
 
 def userInput():
+    print("Welcome to the Epidemic Simulator.\nInput your values for the following variables.")
     pop = int(input("Total population: "))
     infect = int(input("Initial infected: "))
     vaccine = float(input("Percentage of population vaccinated (Enter as decimal): "))
-    disease = input("Type of disease: ")
+    disease = int(input("Type of disease:\n[0] Measles\n[1] Flu\n[2] Tuberculosis\n[3] Cholera\n[4] Ebola\nEnter a number for the disease you want: "))
     days = input("How many days do you want to model: ")
     
     return pop, infect, vaccine, disease, days
 
+a = np.array([["Measles", 0.90], 
+              ["Flu",0.40], 
+              ["Tuberculosis",0.80], 
+              ["Cholera",0.70], 
+              ["Ebola", 0.80]])
+
 def severityCoeff(infect, vaccine, disease):
-    diseaseCoeff = a[disease, 1]
-    infectCoeff = 0.05 * infect
-    sev = diseaseCoeff + infectCoeff - vaccine
+    diseaseCoeff = float(a[disease, 1])
+    sev = diseaseCoeff - vaccine
     return sev
     
 pop, infect, vaccine, disease, days = userInput()
-a = np.array([["Measles", 0.90], ["Flu",0.40], ["Tuberculosis",0.80], ["Cholera",0.70], ["Ebola", 0.80]])
+
 sev = severityCoeff(infect, vaccine, disease)
